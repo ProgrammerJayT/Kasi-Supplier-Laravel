afac825068a419531b671c7886088853df9ca0f4
Programmer JayT <theanthem8@gmail.com>
2022-09-20 08:53:19 +0200

    Added registration dummy view

--- Web/resources/views/auth/register.blade.php
+++ Web/resources/views/auth/register.blade.php
@@ -4,4 +4,53 @@
 
 @section('body')
 
+<body class="bg-gradient-primary">
+    <form action="registration-request" method="post">
+        @csrf
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-10 col-lg-12 col-md-9">
+                    <div class="card o-hidden border-0 shadow-lg my-5">
+                        <div class="card-body p-0">
+                            <div class="row">
+                                <div class="col-lg-6 d-none d-lg-block bg-register-image"></div>
+                                <div class="col-lg-6">
+                                    <div class="p-5">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                                        </div>
+                                        <form class="user">
+                                            <div class="form-group">
+                                                <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Name">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="email" class="form-control form-control-user" id="email" name="email" placeholder="Email Address">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="password" class="form-control form-control-user" id="password_confirmation" name="password_confirmation" placeholder="Repeat Password">
+                                            </div>
+                                            <button type="submit" class="btn btn-primary btn-user btn-block">
+                                                Register Account
+                                            </button>
+                                        </form>
+                                        <hr>
+                                        <div class="text-center">
+                                            <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                        </div>
+                                        <div class="text-center">
+                                            <a class="small" href="login.html">Already have an account? Login!</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+
 @endsection
