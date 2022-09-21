7f2b7af483384b9322ee8a230ddb116d18c56dab
Programmer JayT <theanthem8@gmail.com>
2022-09-21 18:01:28 +0200

    Added validation rules

--- Web/app/Http/Controllers/AuthControl.php
+++ Web/app/Http/Controllers/AuthControl.php
@@ -10,9 +10,12 @@ class AuthControl extends Controller
     public function register(Request $req)
     {
         $req->validate([
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required|min:6|max:12'
+            'name' => 'required|alpha',
+            'surname' => 'required|alpha',
+            'address' => 'required',
+            'email' => 'required|email|unique:account',
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|same:password'
         ]);
 
         return $req->input();
