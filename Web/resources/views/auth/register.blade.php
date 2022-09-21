3970cf23a748e4fa3ffc523167416f8a3d751d53
Programmer JayT <theanthem8@gmail.com>
2022-09-21 18:07:40 +0200

    Added error message alert

--- Web/resources/views/auth/register.blade.php
+++ Web/resources/views/auth/register.blade.php
@@ -4,53 +4,137 @@
 
 @section('body')
 
-<body class="bg-gradient-primary">
-    <form action="registration-request" method="post">
-        @csrf
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-10 col-lg-12 col-md-9">
-                    <div class="card o-hidden border-0 shadow-lg my-5">
-                        <div class="card-body p-0">
-                            <div class="row">
-                                <div class="col-lg-6 d-none d-lg-block bg-register-image"></div>
-                                <div class="col-lg-6">
-                                    <div class="p-5">
-                                        <div class="text-center">
-                                            <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
-                                        </div>
-                                        <form class="user">
-                                            <div class="form-group">
-                                                <input type="text" class="form-control form-control-user" id="name" name="name" placeholder="Name">
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="email" class="form-control form-control-user" id="email" name="email" placeholder="Email Address">
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="password" class="form-control form-control-user" id="password_confirmation" name="password_confirmation" placeholder="Repeat Password">
-                                            </div>
-                                            <button type="submit" class="btn btn-primary btn-user btn-block">
-                                                Register Account
-                                            </button>
-                                        </form>
-                                        <hr>
-                                        <div class="text-center">
-                                            <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                        </div>
-                                        <div class="text-center">
-                                            <a class="small" href="login.html">Already have an account? Login!</a>
+    <body style="background: var(--bs-gray-dark);">
+        <section class="position-relative py-1 py-xl-5">
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col-md-8 col-xl-6 text-center mx-auto">
+                        <h1 class="display-2 text-muted">Create Account</h1>
+                    </div>
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-6 col-xl-4">
+                        <div class="card shadow mb-5" style="margin: 0px -100px 48px;background: var(--bs-gray-dark);">
+                            <div class="card-body d-flex flex-column align-items-center">
+                                <form class="text-center" style="margin-top: 20px;" action="registration-request"
+                                    method="POST">
+                                    @csrf
+
+                                    {{-- Prompt for user's name --}}
+                                    <div class="mb-3">
+                                        <div class="input-group" style="margin: 0px 0px 16px;">
+                                            <span class="input-group-text">
+                                                <i class="far fa-envelope"></i>
+                                            </span>
+                                            <input class="form-control form-control-lg" type="text" name="name"
+                                                placeholder="Name" value="{{ old('name') }}">
                                         </div>
                                     </div>
-                                </div>
+
+                                    {{-- Error section --}}
+                                    @error('name')
+                                        <div>
+                                            <p
+                                                class="text-danger border rounded border-2 border-danger shadow-sm pulse animated">
+                                                {{ $message }}</p>
+                                        </div>
+                                    @enderror
+
+
+
+                                    {{-- Prompt for user's surname --}}
+                                    <div style="margin: 0px 0px 16px;">
+                                        <input class="form-control form-control-lg" type="text" name="surname"
+                                            placeholder="Surname" value="{{ old('surname') }}">
+                                    </div>
+
+                                    @error('surname')
+                                        <div>
+                                            <p
+                                                class="text-danger border rounded border-2 border-danger shadow-sm pulse animated">
+                                                {{ $message }}</p>
+                                        </div>
+                                    @enderror
+
+
+                                    <div class="mb-3">
+                                        <div class="input-group" style="margin: 0px 0px 16px;">
+                                            <span class="input-group-text"><i class="far fa-envelope"></i></span>
+                                            <input class="form-control form-control-lg" type="email" name="email"
+                                                placeholder="Email address">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="mb-3">
+                                        <div class="input-group" style="margin: 0px 0px 16px;"><span
+                                                class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 -32 576 576" width="1em" height="1em"
+                                                    fill="currentColor">
+                                                    <path
+                                                        d="M408 120C408 174.6 334.9 271.9 302.8 311.1C295.1 321.6 280.9 321.6 273.2 311.1C241.1 271.9 168 174.6 168 120C168 53.73 221.7 0 288 0C354.3 0 408 53.73 408 120zM288 152C310.1 152 328 134.1 328 112C328 89.91 310.1 72 288 72C265.9 72 248 89.91 248 112C248 134.1 265.9 152 288 152zM425.6 179.8C426.1 178.6 426.6 177.4 427.1 176.1L543.1 129.7C558.9 123.4 576 135 576 152V422.8C576 432.6 570 441.4 560.9 445.1L416 503V200.4C419.5 193.5 422.7 186.7 425.6 179.8zM150.4 179.8C153.3 186.7 156.5 193.5 160 200.4V451.8L32.91 502.7C17.15 508.1 0 497.4 0 480.4V209.6C0 199.8 5.975 190.1 15.09 187.3L137.6 138.3C140 152.5 144.9 166.6 150.4 179.8H150.4zM327.8 331.1C341.7 314.6 363.5 286.3 384 255V504.3L192 449.4V255C212.5 286.3 234.3 314.6 248.2 331.1C268.7 357.6 307.3 357.6 327.8 331.1L327.8 331.1z">
+                                                    </path>
+                                                </svg></span><input class="form-control form-control-lg" type="text"
+                                                placeholder="Location" name="address">
+                                        </div>
+                                    </div>
+                                    <div class="mb-3">
+                                        <div class="input-group" style="margin: 0px 0px 16px;"><span
+                                                class="input-group-text"><svg xmlns="http://www.w3.org/2000/svg"
+                                                    width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16"
+                                                    class="bi bi-exclamation-square">
+                                                    <path
+                                                        d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z">
+                                                    </path>
+                                                    <path
+                                                        d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z">
+                                                    </path>
+                                                </svg></span>
+                                            <select class="form-select form-select-lg" name="userType">
+                                                <option value="" selected>--Account Type--</option>
+                                                <option value="customer">Customer</option>
+                                                <option value="vendor">Vendor</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="mb-3">
+                                        <div class="input-group">
+                                            <span class="input-group-text">
+                                                <i class="fas fa-lock"></i>
+                                            </span>
+                                            <input class="form-control form-control-lg" type="password" name="password"
+                                                placeholder="Password">
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div class="input-group">
+                                            <span class="input-group-text">
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="-32 0 512 512"
+                                                    width="1em" height="1em" fill="currentColor">
+                                                    <!--! Font Awesome Free 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License) Copyright 2022 Fonticons, Inc. -->
+                                                    <path
+                                                        d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z">
+                                                    </path>
+                                                </svg>
+                                            </span>
+                                            <input class="form-control form-control-lg" type="password"
+                                                name="password_confirmation" placeholder="Retype Password">
+                                        </div>
+                                    </div>
+                                    <div class="mb-3" style="margin: 0px 0px 50px;">
+                                        <button
+                                            class="btn btn-outline-primary fs-5 border rounded-pill border-primary shadow-sm d-block w-100"
+                                            type="submit" style="margin: 60px 0px 16px;">Register Account&nbsp;
+                                        </button>
+                                    </div>
+                                    <p class="text-muted">Already have an account?&nbsp;<a href="">Sign In</a>
+                                    </p>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </form>
+        </section>
 
-@endsection
+    @endsection
