ca32d279eb68f655ea959bd534e753ef020f1616
Programmer JayT <theanthem8@gmail.com>
2022-09-22 17:43:06 +0200

    Added error alert for location

--- Web/resources/views/auth/register.blade.php
+++ Web/resources/views/auth/register.blade.php
@@ -88,18 +88,41 @@
                                                         d="M408 120C408 174.6 334.9 271.9 302.8 311.1C295.1 321.6 280.9 321.6 273.2 311.1C241.1 271.9 168 174.6 168 120C168 53.73 221.7 0 288 0C354.3 0 408 53.73 408 120zM288 152C310.1 152 328 134.1 328 112C328 89.91 310.1 72 288 72C265.9 72 248 89.91 248 112C248 134.1 265.9 152 288 152zM425.6 179.8C426.1 178.6 426.6 177.4 427.1 176.1L543.1 129.7C558.9 123.4 576 135 576 152V422.8C576 432.6 570 441.4 560.9 445.1L416 503V200.4C419.5 193.5 422.7 186.7 425.6 179.8zM150.4 179.8C153.3 186.7 156.5 193.5 160 200.4V451.8L32.91 502.7C17.15 508.1 0 497.4 0 480.4V209.6C0 199.8 5.975 190.1 15.09 187.3L137.6 138.3C140 152.5 144.9 166.6 150.4 179.8H150.4zM327.8 331.1C341.7 314.6 363.5 286.3 384 255V504.3L192 449.4V255C212.5 286.3 234.3 314.6 248.2 331.1C268.7 357.6 307.3 357.6 327.8 331.1L327.8 331.1z">
                                                     </path>
                                                 </svg></span><input class="form-control form-control-lg" type="text"
-                                                placeholder="Location" name="address">
+                                                placeholder="Location" name="location">
                                         </div>
                                     </div>
+
+                                    {{-- Error section --}}
+                                    @error('location')
+                                        <div>
+                                            <p
+                                                class="text-danger border rounded border-2 border-danger shadow-sm pulse animated">
+                                                {{ $message }}</p>
+                                        </div>
+                                    @enderror
+
+
                                     <div class="mb-3">
                                         <div class="input-group" style="margin: 0px 0px 16px;">
-                                            <select class="form-select form-select-lg" name="userType">
+                                            <select class="form-select form-select-lg" name="type">
                                                 <option value="" selected>--Account Type--</option>
                                                 <option value="customer">Customer</option>
                                                 <option value="vendor">Vendor</option>
                                             </select>
                                         </div>
                                     </div>
+
+                                    {{-- Error section --}}
+                                    @error('type')
+                                        <div>
+                                            <p
+                                                class="text-danger border rounded border-2 border-danger shadow-sm pulse animated">
+                                                {{ $message }}</p>
+                                        </div>
+                                    @enderror
+
+
+
                                     <div class="mb-3">
                                         <div class="input-group">
                                             <span class="input-group-text">
@@ -109,6 +132,17 @@
                                                 placeholder="Password">
                                         </div>
                                     </div>
+
+                                    {{-- Error section --}}
+                                    @error('password')
+                                        <div>
+                                            <p
+                                                class="text-danger border rounded border-2 border-danger shadow-sm pulse animated">
+                                                {{ $message }}</p>
+                                        </div>
+                                    @enderror
+
+
                                     <div>
                                         <div class="input-group">
                                             <span class="input-group-text">
@@ -124,6 +158,17 @@
                                                 name="password_confirmation" placeholder="Retype Password">
                                         </div>
                                     </div>
+
+                                    {{-- Error section --}}
+                                    @error('password_confirmation')
+                                        <div>
+                                            <p
+                                                class="text-danger border rounded border-2 border-danger shadow-sm pulse animated">
+                                                {{ $message }}</p>
+                                        </div>
+                                    @enderror
+
+
                                     <div class="mb-3" style="margin: 0px 0px 50px;">
                                         <button
                                             class="btn btn-outline-primary fs-5 border rounded-pill border-primary shadow-sm d-block w-100"
