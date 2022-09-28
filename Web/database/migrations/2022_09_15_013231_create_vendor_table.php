26f40051d6c23149254578c91f05fff78388f577
Programmer JayT <theanthem8@gmail.com>
2022-09-28 19:36:48 +0200

    Removed location attribute

--- Web/database/migrations/2022_09_15_013231_create_vendor_table.php
+++ Web/database/migrations/2022_09_15_013231_create_vendor_table.php
@@ -18,7 +18,6 @@ class CreateVendorTable extends Migration
             $table->string('name');
             $table->string('surname');
             $table->string('email')->unique();
-            $table->string('location');
             $table->timestamps();
         });
     }
