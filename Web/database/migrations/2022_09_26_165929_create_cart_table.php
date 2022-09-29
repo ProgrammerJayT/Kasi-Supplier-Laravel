06d1f7f6e50efb2e914681b319c9daaf1233d62d
Programmer JayT <theanthem8@gmail.com>
2022-09-29 23:41:44 +0200

    Added attributes to the cart table

--- Web/database/migrations/2022_09_26_165929_create_cart_table.php
+++ Web/database/migrations/2022_09_26_165929_create_cart_table.php
@@ -15,6 +15,8 @@ class CreateCartTable extends Migration
     {
         Schema::create('cart', function (Blueprint $table) {
             $table->id();
+            $table->integer('ord_id');
+            $table->integer('item_id');
             $table->timestamps();
         });
     }
