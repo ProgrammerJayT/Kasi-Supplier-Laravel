267448a0c8acf7508d354d96e8f6e87e821e5892
Programmer JayT <theanthem8@gmail.com>
2022-09-29 23:40:48 +0200

    Added add user method for customer, vendor, and administrator account

--- Web/app/Http/Controllers/AdminControl.php
+++ Web/app/Http/Controllers/AdminControl.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
+use App\Models\Admin;
+use App\Models\Customer;
+use App\Models\Vendor;
+use Illuminate\Support\Facades\Hash;
 
 class AdminControl extends Controller
 {
     //
+    public function createUser(Request $req) {
+        
+        $req->validate([
+            'name' => 'required|alpha',
+            'surname' => 'required|alpha',
+            'email' => 'required|email|unique:account',
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|min:8|same:password',
+        ]);
+
+        if ($req->type == 'administrator') {
+            $model = new Admin;
+            $active = true;
+            $role = 'admin';
+        } else {
+            $req->input('type') == 'customer' ? [$model = new Customer, $role = $req->input('type')]
+                : [$model = new Vendor, $role = $req->input('type')];
+            $active = false;
+        }
+
+        //Store user's details
+        $model->name = ucfirst(strtolower($req->input('name')));
+        $model->surname = ucfirst(strtolower($req->input('surname')));
+        $model->email = strtolower($req->input('email'));
+
+        //Create user's account
+        $account = new Account;
+        $account->email = strtolower($req->input('email'));
+        $account->password = Hash::make($req->input('password'));
+        $account->type = $role;
+        $account->active = $active;
+
+        if ($model->save() && $account->save()) {
+            return back()->with('success', ucfirst($req->type).' account created successfully');
+        } else {
+            return back()->with('fail', 'Your account could not be created');
+        }
+
+    }
 }
