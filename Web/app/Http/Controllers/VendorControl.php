66b13f63f6b3e51fe68d1f2cb077ea46d00cc73f
Programmer JayT <theanthem8@gmail.com>
2022-09-30 00:47:18 +0200

    Added add product view method

--- Web/app/Http/Controllers/VendorControl.php
+++ Web/app/Http/Controllers/VendorControl.php
@@ -7,4 +7,11 @@ use Illuminate\Http\Request;
 class VendorControl extends Controller
 {
     //
+    public function addProduct() {
+        return view('vendor-ops.add-product');
+    }
+
+    public function addProductRequest(Request $req) {
+        
+    }
 }
