c5ca35b14e540216862203c5fd08a5d68ba43b54
Programmer JayT <theanthem8@gmail.com>
2022-09-29 23:38:46 +0200

    Modified admin dashboard

--- Web/resources/views/dashboard/admin.blade.php
+++ Web/resources/views/dashboard/admin.blade.php
@@ -1,141 +1,47 @@
-@extends('components.layout')
+@extends('components.admin-layout')
 
 @section('title', 'Dashboard')
 
 @section('body')
 
-    <body>
-        <div class="container">
-            <h1>Account Approval<span style="margin-left: 25px;"></span></h1>
-        </div>
-        <div class="container" style="margin-top: 50px;width: 1500px;">
-            <div class="card shadow-lg" style="height: 650px;">
-                <div class="card-header justify-content-center align-items-center align-content-center py-3"><span
-                        class="fs-5 fw-lighter text-start text-primary">Wishlist Info</span></div>
-                <div class="card-body" style="background: #ffffff;">
-                    <div class="row">
-                        <div class="col-md-6 text-nowrap">
-                            <div id="dataTable_length" class="dataTables_length" aria-controls="dataTable"><label
-                                    class="form-label">Show&nbsp;
-                                    <select class="d-inline-block form-select form-select-sm">
-                                        <option value="3" selected="">5</option>
-                                        <option value="10">10</option>
-                                        <option value="15">15</option>
-                                        <option value="20">20</option>
-                                    </select>&nbsp;</label>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="text-md-end dataTables_filter" id="dataTable_filter"><label
-                                    class="form-label"><input type="search" class="form-control form-control-sm"
-                                        aria-controls="dataTable" placeholder="Search"></label></div>
-                        </div>
-                    </div>
-                    <div class="table-responsive table mt-2" id="dataTable-1" role="grid"
-                        aria-describedby="dataTable_info">
-                        <table class="table my-0" id="dataTable">
-                            <thead>
-                                <tr>
-                                    <th style="width: 350px;">Account Type</th>
-                                    <th style="width: 250px;height: 39px;margin-top: 0px;padding-top: 21px;">First Name
-                                    </th>
-                                    <th style="width: 250px;">Last Name</th>
-                                    <th style="width: 250px;">Address</th>
-                                    <th style="width: 151.797px;">Date</th>
-                                    <th style="width: 97.5469px;">Option</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr class="table-light justify-content-center" style="height: 1px;">
-                                    <td style="padding-top: 35px;">Vendor</td>
-                                    <td style="padding-top: 35px;width: 146.5px;">Terry</td>
-                                    <td style="padding-top: 35px;">Myers</td>
-                                    <td style="padding-top: 35px;">Pretoria 125str</td>
-                                    <td style="padding-top: 35px;">2008/11/28</td>
-                                    <td style="padding-top: 10px;padding-right: 10px;padding-left: 0px;"><button
-                                            class="btn btn-outline-success btn-sm border rounded-pill border-success"
-                                            type="button"
-                                            style="padding-bottom: 4px;margin-bottom: 10px;padding-top: 4px;padding-right: 20px;padding-left: 20px;">Approve</button><button
-                                            class="btn btn-outline-danger btn-sm border rounded-pill border-danger"
-                                            type="button"
-                                            style="margin-right: 10px;padding-right: 20px;padding-left: 20px;">Remove&nbsp;</button>
-                                    </td>
-                                </tr>
-                                <tr class="table-light justify-content-center" style="height: 1px;">
-                                    <td style="padding-top: 35px;">Customer</td>
-                                    <td style="padding-top: 35px;width: 146.5px;">Terry</td>
-                                    <td style="padding-top: 35px;">Myers</td>
-                                    <td style="padding-top: 35px;">Pretoria 125str</td>
-                                    <td style="padding-top: 35px;">2008/11/28</td>
-                                    <td style="padding-top: 10px;padding-right: 10px;padding-left: 0px;"><button
-                                            class="btn btn-outline-success btn-sm border rounded-pill border-success"
-                                            type="button"
-                                            style="padding-bottom: 4px;margin-bottom: 10px;padding-top: 4px;padding-right: 20px;padding-left: 20px;">Approve</button><button
-                                            class="btn btn-outline-danger btn-sm border rounded-pill border-danger"
-                                            type="button"
-                                            style="margin-right: 10px;padding-right: 20px;padding-left: 20px;">Remove&nbsp;</button>
-                                    </td>
-                                </tr>
-                                <tr class="table-light justify-content-center" style="height: 1px;">
-                                    <td style="padding-top: 35px;">Vendor</td>
-                                    <td style="padding-top: 35px;width: 146.5px;">Terry</td>
-                                    <td style="padding-top: 35px;">Myers</td>
-                                    <td style="padding-top: 35px;">Pretoria 125str</td>
-                                    <td style="padding-top: 35px;">2008/11/28</td>
-                                    <td style="padding-top: 10px;padding-right: 10px;padding-left: 0px;"><button
-                                            class="btn btn-outline-success btn-sm border rounded-pill border-success"
-                                            type="button"
-                                            style="padding-bottom: 4px;margin-bottom: 10px;padding-top: 4px;padding-right: 20px;padding-left: 20px;">Approve</button><button
-                                            class="btn btn-outline-danger btn-sm border rounded-pill border-danger"
-                                            type="button"
-                                            style="margin-right: 10px;padding-right: 20px;padding-left: 20px;">Remove&nbsp;</button>
-                                    </td>
-                                </tr>
-                                <tr class="table-light justify-content-center" style="height: 1px;">
-                                    <td style="padding-top: 35px;">Customer</td>
-                                    <td style="padding-top: 35px;width: 146.5px;">Terry</td>
-                                    <td style="padding-top: 35px;">Myers</td>
-                                    <td style="padding-top: 35px;">Pretoria 125str</td>
-                                    <td style="padding-top: 35px;">2008/11/28</td>
-                                    <td style="padding-top: 10px;padding-right: 10px;padding-left: 0px;"><button
-                                            class="btn btn-outline-success btn-sm border rounded-pill border-success"
-                                            type="button"
-                                            style="padding-bottom: 4px;margin-bottom: 10px;padding-top: 4px;padding-right: 20px;padding-left: 20px;">Approve</button><button
-                                            class="btn btn-outline-danger btn-sm border rounded-pill border-danger"
-                                            type="button"
-                                            style="margin-right: 10px;padding-right: 20px;padding-left: 20px;">Remove&nbsp;</button>
-                                    </td>
-                                </tr>
-                                <tr class="table-light justify-content-center" style="height: 1px;">
-                                    <td style="padding-top: 35px;">Vendor</td>
-                                    <td style="padding-top: 35px;width: 146.5px;">Terry</td>
-                                    <td style="padding-top: 35px;">Myers</td>
-                                    <td style="padding-top: 35px;">Pretoria 125str</td>
-                                    <td style="padding-top: 35px;">2008/11/28</td>
-                                    <td style="padding-top: 10px;padding-right: 10px;padding-left: 0px;"><button
-                                            class="btn btn-outline-success btn-sm border rounded-pill border-success"
-                                            type="button"
-                                            style="padding-bottom: 4px;margin-bottom: 10px;padding-top: 4px;padding-right: 20px;padding-left: 20px;">Approve</button><button
-                                            class="btn btn-outline-danger btn-sm border rounded-pill border-danger"
-                                            type="button"
-                                            style="margin-right: 10px;padding-right: 20px;padding-left: 20px;">Remove&nbsp;</button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <td><strong><br><strong>Account Type</strong><br><br></strong></td>
-                                    <td><strong><br><strong>First Name</strong><br><br></strong></td>
-                                    <td><strong><br><strong>Last Name</strong><br><br></strong></td>
-                                    <td><strong>Address</strong></td>
-                                    <td><strong><br><strong>Date</strong><br><br></strong></td>
-                                    <td><strong>Option</strong></td>
-                                </tr>
-                            </tfoot>
-                        </table>
+        <div class="d-flex d-xxl-flex flex-column align-items-center align-items-xxl-center"
+            style="width: 100%;margin-top: 50px;">
+            <div class="container py-4 py-xl-5">
+                <div class="row mb-5">
+                    <div class="col-md-8 col-xl-6 text-center mx-auto">
+                        <h2 style="text-align: right;color: var(--bs-light);">Features</h2>
+                        <p class="w-lg-50" style="text-align: left;color: var(--bs-gray-500);">With your account, you can
+                            perform the following operations without restrictions. Click the menu button&nbsp;<i
+                                class="icon ion-android-menu"
+                                style="color: var(--km-bg);font-size: 15px;background: #fd3838;padding: 5px;border-radius: 5px;"></i>&nbsp;placed
+                            at the top-right of the screen<br></p>
                     </div>
                 </div>
-                <div class="card-header"></div>
+                <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3">
+                    <div class="col">
+                        <div class="p-4"><span class="badge bg-dark mb-2" style="background: var(--km-black);">Tip</span>
+                            <h4 style="color: var(--bs-gray-500);">Manage Administrator(s)</h4>
+                            <p style="color: var(--bs-gray-500);">You can view,&nbsp; and add new administrator account<br>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="p-4"><span class="badge bg-dark mb-2" style="background: var(--km-black);">Tip</span>
+                            <h4 style="color: var(--bs-gray-500);">Manage Customer(s)</h4>
+                            <p style="color: var(--bs-gray-500);">You can view, edit, delete, and add new customer account
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="p-4"><span class="badge bg-dark mb-2"
+                                style="background: var(--km-black);">Tip</span>
+                            <h4 style="color: var(--bs-gray-500);"><strong><span style="color: var(--bs-gray-500);">Manage
+                                        Vendor(s)</span></strong><br></h4>
+                            <p style="color: var(--bs-gray-500);">You can view, edit, delete, and add new vendor
+                                account<br></p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     @endsection
