98904c0f0e4a0cedfaa376e56b760359732de95b
Programmer JayT <theanthem8@gmail.com>
2022-09-30 03:54:50 +0200

    Mapped categories from database

--- Web/resources/views/dashboard/vendor.blade.php
+++ Web/resources/views/dashboard/vendor.blade.php
@@ -70,128 +70,44 @@
         </div>
         <div class="container">
             <div>
-                <form>
-                    <div class="align-items-center align-content-center">
-                        <div class="input-group input-group-lg shadow"><span class="input-group-text"><svg
-                                    xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-                                    viewBox="0 0 16 16" class="bi bi-search">
-                                    <path
-                                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z">
-                                    </path>
-                                </svg></span><input class="form-control form-control-sm" type="text"
-                                placeholder="Search For Product" name="product_search"><select class="form-select">
-                                <option value="all" selected="">All Departments</option>
-                                <option value="Baby">Baby &amp; Toddler</option>
-                                <option value="Beauty">Beauty</option>
-                                <option value="">Books</option>
-                                <option value="Electronics">Electronics</option>
-                                <option value="Camping">Camping &amp; Outdoors</option>
-                                <option value="">Cellphones</option>
-                                <option value="">Fashion</option>
-                                <option value="Food ">Food &amp; Bevarages</option>
-                                <option value="Garden">Garden, Pool &amp; Patio</option>
-                                <option value="Health">Health &amp; Fitness</option>
-                                <option value="Home">Home &amp; kitchen</option>
-                                <option value="Luggage">Luggage &amp; Travel</option>
-                                <option value="Movies">Movies &amp; Series</option>
-                                <option value="Music">Music</option>
-                                <option value="Office">Office &amp; Stationery</option>
-                                <option value="">Pets</option>
-                                <option value="Sports">Sports</option>
-                                <option value="">Toys</option>
-                            </select><button class="btn btn-outline-warning border rounded-0 border-warning"
-                                type="submit">Search</button></div>
+                <div class="align-items-center align-content-center">
+                    <div class="input-group input-group-lg shadow"><span class="input-group-text"><svg
+                                xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
+                                viewBox="0 0 16 16" class="bi bi-search">
+                                <path
+                                    d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z">
+                                </path>
+                            </svg></span><input class="form-control form-control-sm" type="text"
+                            placeholder="Search For Product" name="product_search"><select class="form-select">
+                            <option value="all" selected="">All Departments</option>
+
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->value }}">{{ $category->name }}</option>
+                            @endforeach
+
+                        </select>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
         <div class="container" style="padding-top: 40px;">
             <div>
                 <div class="row">
-                    <div class="col text-start" style="padding-bottom: 50px;">
-                        <a href="#">
-                            <div class="card" data-bss-hover-animate="pulse" style="width: 300px;">
-                                <div class="card-body">
-                                    <div>
-                                        <img class="rounded img-fluid bg-dark border border-dark shadow"
-                                            data-bs-toggle="tooltip" data-bss-tooltip=""
-                                            style="width: 270px;height: 237px;"
-                                            src="assets/img/223-2238298_4k-wallpaper-for-ps4.jpg" title="Product name">
-                                    </div>
-                                    <div>
-                                        <h3 class="fw-lighter text-start"
-                                            style="width: 201px;height: 58px;margin: 0px 0px 10px;padding: 2px 0px 0px;">
-                                            Black-Belt</h3>
-                                        <p class="text-start text-muted">Gaming Skills Book</p><span class="fw-bolder">R
-                                            20.00</span>
-                                        <div><button
-                                                class="btn btn-outline-success border rounded-pill border-success shadow"
-                                                type="button" style="margin-top: 6px;">Add To Cart</button><button
-                                                class="btn btn-outline-danger border rounded-pill border-danger shadow"
-                                                type="button"
-                                                style="margin-top: 6px;margin-right: 0px;margin-left: 20px;">Add To
-                                                Wishlist</button></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col text-start" style="padding-bottom: 50px;">
-                        <a href="#">
-                            <div class="card" data-bss-hover-animate="pulse" style="width: 300px;">
-                                <div class="card-body">
-                                    <div>
-                                        <img class="rounded img-fluid bg-dark border border-dark shadow"
-                                            data-bs-toggle="tooltip" data-bss-tooltip=""
-                                            style="width: 270px;height: 237px;"
-                                            src="assets/img/223-2238298_4k-wallpaper-for-ps4.jpg" title="Product name">
-                                    </div>
-                                    <div>
-                                        <h3 class="fw-lighter text-start"
-                                            style="width: 201px;height: 58px;margin: 0px 0px 10px;padding: 2px 0px 0px;">
-                                            Black-Belt</h3>
-                                        <p class="text-start text-muted">Gaming Skills Book</p><span class="fw-bolder">R
-                                            20.00</span>
-                                        <div><button
-                                                class="btn btn-outline-success border rounded-pill border-success shadow"
-                                                type="button" style="margin-top: 6px;">Add To Cart</button><button
-                                                class="btn btn-outline-danger border rounded-pill border-danger shadow"
-                                                type="button"
-                                                style="margin-top: 6px;margin-right: 0px;margin-left: 20px;">Add To
-                                                Wishlist</button></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col text-start" style="padding-bottom: 50px;">
-                        <a href="#">
-                            <div class="card" data-bss-hover-animate="pulse" style="width: 300px;">
-                                <div class="card-body">
-                                    <div>
-                                        <img class="rounded img-fluid bg-dark border border-dark shadow"
-                                            data-bs-toggle="tooltip" data-bss-tooltip=""
-                                            style="width: 270px;height: 237px;"
-                                            src="assets/img/223-2238298_4k-wallpaper-for-ps4.jpg" title="Product name">
-                                    </div>
-                                    <div>
-                                        <h3 class="fw-lighter text-start"
-                                            style="width: 201px;height: 58px;margin: 0px 0px 10px;padding: 2px 0px 0px;">
-                                            Black-Belt</h3>
-                                        <p class="text-start text-muted">Gaming Skills Book</p><span class="fw-bolder">R
-                                            20.00</span>
-                                        <div><button
-                                                class="btn btn-outline-success border rounded-pill border-success shadow"
-                                                type="button" style="margin-top: 6px;">Add To Cart</button><button
-                                                class="btn btn-outline-danger border rounded-pill border-danger shadow"
-                                                type="button"
-                                                style="margin-top: 6px;margin-right: 0px;margin-left: 20px;">Add To
-                                                Wishlist</button></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+
+                    @unless(count($items) == 0)
+                        @foreach ($categories as $category)
+                            @foreach ($items as $item)
+                                @if ($category->id == $item->cat_id && $item->ven_id != Session::get('user'))
+
+                                @endif
+                            @endforeach
+                        @endforeach
+                    @else
+                        <div>
+                            <h1>No products were found</h1>
+                        </div>
+                    @endunless
+
                 </div>
             </div>
         </div>
