e04b192881b1792a7934e917c8660a13582dbdca
Programmer JayT <theanthem8@gmail.com>
2022-09-30 03:53:52 +0200

    Added categories collection

--- Web/app/Http/Controllers/VendorControl.php
+++ Web/app/Http/Controllers/VendorControl.php
@@ -13,7 +13,11 @@ class VendorControl extends Controller
     //
     public function addProduct()
     {
-        return view('vendor-ops.create-product');
+        $category = Category::all();
+
+        return view('vendor-ops.create-product', [
+            'categories' => $category
+        ]);
     }
 
     public function addProductRequest(Request $req)
@@ -27,18 +31,35 @@ class VendorControl extends Controller
         // ]);
 
         $req->validate([
-            'product_name' => 'required',
-            'product_description' => 'required | max:100',
+            'product_name' => 'required', //Sorted
+            'product_description' => 'required | max:100', //Sorted
             'product_price' => 'required | numeric',
             'product_image' => 'required',
             'department_Type' => 'required'
         ]);
 
-        $vendor = new Vendor;
+
         $item = new Item;
         $category = new Category;
-        
-        
 
+        $getItem = $category::where('value', $req->department_Type)->first();
+
+        //cat_id	desc	name	price
+
+        $item->cat_id = $getItem->id;
+        $item->price = $req->product_price;
+        $item->desc = $req->product_description;
+        $item->name = ucfirst(strtolower($req->product_name));
+        $item->ven_id = session()->get('user');
+
+        $item->save() ? [
+            $type = 'success',
+            $message = 'Product added successfully',
+        ] : [
+            $type = 'fail',
+            $message = 'Could not create product',
+        ];
+
+        return back()->with($type, $message);
     }
 }
