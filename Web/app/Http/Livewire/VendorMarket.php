42821b235ba2e03b8e39e90440ce06310217e079
Programmer JayT <theanthem8@gmail.com>
2022-10-04 19:54:02 +0200

    Created livewire vendor market handlers

---
+++ Web/app/Http/Livewire/VendorMarket.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class VendorMarket extends Component
+{
+    public function render()
+    {
+        return view('livewire.vendor-market');
+    }
+}
