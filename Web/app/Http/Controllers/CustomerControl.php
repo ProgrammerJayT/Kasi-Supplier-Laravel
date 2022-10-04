c7154b1db30ba694d7f6e2cac947c1acdb5d3243
Programmer JayT <theanthem8@gmail.com>
2022-10-04 19:19:09 +0200

    Added customer dashboard method

--- Web/app/Http/Controllers/CustomerControl.php
+++ Web/app/Http/Controllers/CustomerControl.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerControl extends Controller
 {
     //
+    public function dashboard() {
+        
+        $user = Customer::where('id', session()->get('user'))->first();
+        $name = $user->name;
+        $surname = $user->surname;
+        $fullName = $name . " " . $surname;
+        $dateJoined = $user->created_at->toDateString();
+
+        return view('dashboard.customer', [
+            'name' => $name,
+            'surname' => $surname,
+            'fullName' => $fullName,
+            'dateJoined' => $dateJoined,
+        ]);
+    }
 }
