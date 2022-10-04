323e371e0c11f6e71529151a5b0271ffc27dc046
Programmer JayT <theanthem8@gmail.com>
2022-10-04 22:14:13 +0200

    Added data filters

--- Web/app/Http/Livewire/CustomerMarket.php
+++ Web/app/Http/Livewire/CustomerMarket.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
+use App\Models\Item;
 use Livewire\Component;
 
 class CustomerMarket extends Component
 {
+    public $search = '';
+    public $category = '';
+
     public function render()
     {
-        return view('livewire.customer-market');
+
+        return view('livewire.customer-market', [
+            'items' => Item::where('name', 'like', '%' . $this->search . '%')->get(),
+            'categories' => Category::where('name', 'like', '%' . $this->category . '%')->get(),
+            'categoryList' => Category::all(),
+        ]);
     }
 }
