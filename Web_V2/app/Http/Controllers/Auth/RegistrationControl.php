50c78f870e12574500ba4c1032991c080c1d2403
Programmer JayT <theanthem8@gmail.com>
2022-10-07 18:34:52 +0200

    Registration complete

--- Web_V2/app/Http/Controllers/Auth/RegistrationControl.php
+++ Web_V2/app/Http/Controllers/Auth/RegistrationControl.php
@@ -8,11 +8,37 @@ use Illuminate\Http\Request;
 class RegistrationControl extends Controller
 {
     //
-    public function register() {
+    public function register()
+    {
         return view('auth-ops.register');
     }
-    
-    public function registerRequest(Request $request) {
-        return $request->all();
+
+    public function registerRequest(Request $request)
+    {
+
+        $request->validate([
+            'name' => 'required|alpha',
+            'surname' => 'required|alpha',
+            'type' => 'required',
+            'email' => 'required|email|unique:account',
+            'password' => 'required|min:8',
+            'password_confirmation' => 'required|same:password',
+        ]);
+
+        $id = AccountControl::create(
+            $request->name,
+            $request->surname,
+            $request->email,
+            $request->password,
+            $request->type
+        );
+
+        if ($id[1] == true) {
+
+            session()->put('user', $id[0]);
+            return redirect('/' . $request->type . '-dashboard');
+        } else {
+            return back()->with('fail', 'Failed to create your account');
+        }
     }
 }
