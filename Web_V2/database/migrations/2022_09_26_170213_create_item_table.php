72e4d98d54b1e3b44b2b542b93cc60467f1841f0
Programmer JayT <theanthem8@gmail.com>
2022-10-07 18:36:14 +0200

    Created migrations

---
+++ Web_V2/database/migrations/2022_09_26_170213_create_item_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cat_id');
+            $table->integer('ven_id');
+            $table->string('desc');
+            $table->string('name');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item');
+    }
+}
