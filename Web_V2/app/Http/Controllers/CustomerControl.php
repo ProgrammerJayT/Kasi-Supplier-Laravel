3c9160bc19748ba6103a6f6a0d425f313b27800f
Programmer JayT <theanthem8@gmail.com>
2022-10-08 13:26:30 +0200

    Created index, dashboard, profile, and update methods

--- Web_V2/app/Http/Controllers/CustomerControl.php
+++ Web_V2/app/Http/Controllers/CustomerControl.php
@@ -2,17 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerControl extends Controller
 {
     //
-    public function dashboard()
+    public function index()
     {
-        return view('dashboard.customer-dashboard');
+        $user = Customer::where('id', session()->get('user'))->first();
+        return $user;
     }
 
-    public function profile() {
-        
+    public function dashboard()
+    {
+
+        return view('dashboard.customer-dashboard', [
+            'name' => self::index()->name,
+            'surname' => self::index()->surname,
+            'email' => self::index()->email,
+            'image' => self::index()->image,
+            'date' => self::index()->created_at->diffForHumans(),
+        ]);
+    }
+
+    public function profile()
+    {
+        return view('profile.customer-profile', [
+            'name' => self::index()->name,
+            'surname' => self::index()->surname,
+            'email' => self::index()->email,
+            'image' => self::index()->image,
+            'date' => self::index()->created_at->diffForHumans(),
+        ]);
+    }
+
+    public function update(Request $request) {
+        return $request->all();
     }
 }
