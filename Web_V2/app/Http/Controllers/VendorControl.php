3aac0463ae388b2e5d041f8d05802d94678517c1
Programmer JayT <theanthem8@gmail.com>
2022-10-10 12:12:14 +0200

    Added indexing method

--- Web_V2/app/Http/Controllers/VendorControl.php
+++ Web_V2/app/Http/Controllers/VendorControl.php
@@ -2,21 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Item;
 use App\Models\Vendor;
 use Illuminate\Http\Request;
 
 class VendorControl extends Controller
 {
     //
+
+    public function index()
+    {
+        $vendors = Vendor::where('id', session()->get('user'))->first();
+
+        return $vendors;
+    }
+
     public function dashboard()
     {
-        $user = Vendor::where('id', session()->get('user'))->first();
         return view('dashboard.vendor-dashboard', [
-            'name' => $user->name
+            'name' => self::index()->name,
+            'surname' => self::index()->surname,
+            'email' => self::index()->email,
+            'image' => self::index()->image,
+            'date' => self::index()->created_at->diffForHumans(),
+            'items' => Item::where('ven_id', session()->get('user'))->count(),
         ]);
     }
 
-    public function profile() {
-        return view('profile.vendor-profile');
+    public function profile()
+    {
+        return view('profile.vendor-profile', [
+            'name' => self::index()->name,
+            'surname' => self::index()->surname,
+            'email' => self::index()->email,
+            'image' => self::index()->image,
+            'date' => self::index()->created_at->diffForHumans(),
+        ]);
     }
 }
