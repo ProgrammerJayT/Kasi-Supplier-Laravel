689c40d0f062393410a3a8330745dbda8e348ed9
Programmer JayT <theanthem8@gmail.com>
2022-10-10 12:23:10 +0200

    Added image path and user's name section for yielding

--- Web_V2/resources/views/dashboard/vendor-dashboard.blade.php
+++ Web_V2/resources/views/dashboard/vendor-dashboard.blade.php
@@ -1,6 +1,8 @@
 @extends('components.vendor-layout')
 
 @section('title', 'Vendor Dashboard')
+@section('image-path', $image)
+@section('username', $name)
 
 @section('body')
 
@@ -8,7 +10,6 @@
         <div class="row d-flex justify-content-center">
             <div class="col-lg-9 text-center">
                 <div class="blog__hero__text">
-                    <h3>Welcome back {{$name}}</h3>
                 </div>
             </div>
         </div>
@@ -21,46 +22,30 @@
                     <a href=""><img src="img/logo.png" alt=""></a>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-6">
-                <nav class="header__menu mobile-menu">
-                    <ul>
-                        <li class="active"><a href="customer-dashboard">Home</a></li>
-                        <li><a href="./shop.html">Shop</a></li>
-                        <li><a href="#">Pages</a>
-                            <ul class="dropdown">
-                                <li><a href="./about.html">About Us</a></li>
-                                <li><a href="./shop-details.html">Shop Details</a></li>
-                                <li><a href="./shopping-cart.html">Shopping Cart</a></li>
-                                <li><a href="./checkout.html">Check Out</a></li>
-                                <li><a href="./blog-details.html">Blog Details</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="./blog.html">Blog</a></li>
-                        <li><a href="./contact.html">Profile</a></li>
-                    </ul>
-                </nav>
-            </div>
+
         </div>
         <div class="canvas__open"><i class="fa fa-bars"></i></div>
     </div>
 
-    <div class="row">
+    <div class="row" style="margin-top: 50px; display: flex;align-items: center;justify-content: center;">
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-            <div class="card">
-                <div class="card-header p-3 pt-2">
-                    <div
-                        class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
-                        <i class="material-icons opacity-10">weekend</i>
+            <a href="">
+                <div class="card">
+                    <div class="card-header p-3 pt-2">
+                        <div
+                            class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
+                            <i class="material-icons opacity-10">weekend</i>
+                        </div>
+                        <div class="text-end pt-1">
+                            <p class="text-sm mb-0 text-capitalize">Total Orders</p>
+                            <h4 class="mb-0">0</h4>
+                        </div>
                     </div>
-                    <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Pending Orders</p>
-                        <h4 class="mb-0">0</h4>
+                    <hr class="dark horizontal my-0">
+                    <div class="card-footer p-3">
                     </div>
                 </div>
-                <hr class="dark horizontal my-0">
-                <div class="card-footer p-3">
-                </div>
-            </div>
+            </a>
         </div>
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
             <div class="card">
@@ -79,6 +64,9 @@
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="row" style="margin-top: 50px; display: flex;align-items: center;justify-content: center;">
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
             <div class="card">
                 <div class="card-header p-3 pt-2">
@@ -87,7 +75,7 @@
                         <i class="material-icons opacity-10">person</i>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">New Clients</p>
+                        <p class="text-sm mb-0 text-capitalize">Pending Orders</p>
                         <h4 class="mb-0">0</h4>
                     </div>
                 </div>
@@ -104,7 +92,44 @@
                         <i class="material-icons opacity-10">weekend</i>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Sales</p>
+                        <p class="text-sm mb-0 text-capitalize">Wishlist items</p>
+                        <h4 class="mb-0">0</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="row" style="margin-top: 50px; display: flex;align-items: center;justify-content: center;">
+        <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+            <div class="card">
+                <div class="card-header p-3 pt-2">
+                    <div
+                        class="icon icon-lg icon-shape bg-gradient-success shadow-success text-center border-radius-xl mt-n4 position-absolute">
+                        <i class="material-icons opacity-10">person</i>
+                    </div>
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize">My Products</p>
+                        <h4 class="mb-0">{{$items}}</h4>
+                    </div>
+                </div>
+                <hr class="dark horizontal my-0">
+                <div class="card-footer p-3">
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6">
+            <div class="card">
+                <div class="card-header p-3 pt-2">
+                    <div
+                        class="icon icon-lg icon-shape bg-gradient-info shadow-info text-center border-radius-xl mt-n4 position-absolute">
+                        <i class="material-icons opacity-10">weekend</i>
+                    </div>
+                    <div class="text-end pt-1">
+                        <p class="text-sm mb-0 text-capitalize">Sold Products</p>
                         <h4 class="mb-0">0</h4>
                     </div>
                 </div>
