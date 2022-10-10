99cbb863d821d0977c18e0abe6f10078dcb4d26d
Programmer JayT <theanthem8@gmail.com>
2022-10-10 12:22:33 +0200

    Added section for user's name yielding

--- Web_V2/resources/views/dashboard/customer-dashboard.blade.php
+++ Web_V2/resources/views/dashboard/customer-dashboard.blade.php
@@ -2,6 +2,7 @@
 
 @section('title', 'Customer Dashboard')
 @section('image-path', $image)
+@section('username', $name)
 
 @section('body')
 
@@ -21,30 +22,12 @@
                     <a href=""><img src="img/logo.png" alt=""></a>
                 </div>
             </div>
-            <div class="col-lg-6 col-md-6">
-                <nav class="header__menu mobile-menu">
-                    <ul>
-                        <li class="active"><a href="customer-dashboard">Home</a></li>
-                        <li><a href="./shop.html">Shop</a></li>
-                        <li><a href="#">Pages</a>
-                            <ul class="dropdown">
-                                <li><a href="./about.html">About Us</a></li>
-                                <li><a href="./shop-details.html">Shop Details</a></li>
-                                <li><a href="./shopping-cart.html">Shopping Cart</a></li>
-                                <li><a href="./checkout.html">Check Out</a></li>
-                                <li><a href="./blog-details.html">Blog Details</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="./blog.html">Blog</a></li>
-                        <li><a href="./contact.html">Profile</a></li>
-                    </ul>
-                </nav>
-            </div>
+
         </div>
         <div class="canvas__open"><i class="fa fa-bars"></i></div>
     </div>
 
-    <div class="row">
+    <div class="row" style="margin-top: 50px; display: flex;align-items: center;justify-content: center;">
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
             <div class="card">
                 <div class="card-header p-3 pt-2">
@@ -53,7 +36,7 @@
                         <i class="material-icons opacity-10">weekend</i>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Pending Orders</p>
+                        <p class="text-sm mb-0 text-capitalize">Total Orders</p>
                         <h4 class="mb-0">0</h4>
                     </div>
                 </div>
@@ -79,6 +62,9 @@
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="row" style="margin-top: 50px; display: flex;align-items: center;justify-content: center;">
         <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
             <div class="card">
                 <div class="card-header p-3 pt-2">
@@ -87,7 +73,7 @@
                         <i class="material-icons opacity-10">person</i>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">New Clients</p>
+                        <p class="text-sm mb-0 text-capitalize">Pending Clients</p>
                         <h4 class="mb-0">0</h4>
                     </div>
                 </div>
@@ -104,7 +90,7 @@
                         <i class="material-icons opacity-10">weekend</i>
                     </div>
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Sales</p>
+                        <p class="text-sm mb-0 text-capitalize">Wishlist items</p>
                         <h4 class="mb-0">0</h4>
                     </div>
                 </div>
