02e3778dee197196f888e62d3b9eef23b664d99a
Programmer JayT <theanthem8@gmail.com>
2022-10-11 12:12:30 +0200

    Added remove item from cart alert message

--- Web_V2/resources/views/livewire/cart.blade.php
+++ Web_V2/resources/views/livewire/cart.blade.php
@@ -6,6 +6,12 @@
             <div class="row">
                 <div class="col-lg-8">
 
+                    @if (Session::has('item-remove') && count($cartItems) > 0)
+                        <div class="alert alert-success" role="alert">
+                            {{ Session::get('item-remove') }}
+                        </div>
+                    @endif
+
                     @unless(count($cartItems) > 0)
                         <div class="alert alert-warning">
                             <h4 class="alert-heading">No items in cart!</h4>
@@ -19,19 +25,13 @@
                         @foreach ($items as $key => $item)
                             @for ($i = 0; $i < count($cartItems); $i++)
                                 @if ($item->id == $cartItems[$i])
-                                    @php
-                                        print_r($cartItems);
-                                        echo '<br>';
-                                        // print_r($totals);
-                                    @endphp
-
                                     <div class="shopping__cart__table">
                                         <table>
                                             <thead>
                                                 <tr>
                                                     <th>Product</th>
                                                     <th>Quantity</th>
-                                                    {{-- <th>Total</th> --}}
+                                                    <th>Total</th>
                                                     <th></th>
                                                 </tr>
                                             </thead>
@@ -52,12 +52,12 @@
                                                         <div class="quantity" style="width:50%;">
                                                             <input
                                                                 style="width:50%;border-radius:10px;border-color:darkgray;"
-                                                                type="numeric"
-                                                                wire:change="setQuantity({{ $key }})"
-                                                                value="1">
-                                                        </div>
+                                                                type="number" name="{{ $item->id }}"
+                                                                wire:model="quantity.{{ $item->id }}" id="quantity"
+                                                                min=1 </div>
                                                     </td>
-                                                    {{-- <td class="cart__price">R{{ $item->price * $totals[$key] }} --}}
+
+                                                    <td class="cart__price">R{{ $quantity[$item->id] * $item->price }}</td>
                                                     </td>
                                                     <td class="cart__close"><i wire:click="removeItem({{ $item->id }})"
                                                             class="fa fa-close"></i></td>
@@ -73,14 +73,26 @@
                     <div class="row">
                         <div class="col-lg-6 col-md-6 col-sm-6">
                             <div class="continue__btn">
-                                <a href="#">Continue Shopping</a>
+                                <a
+                                    href="{{ route('shopping', [
+                                        'user' => $user,
+                                    ]) }}">Continue
+                                    Shopping</a>
                             </div>
                         </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6">
-                            <div class="continue__btn update__btn">
-                                <a href="#"><i class="fa fa-spinner"></i> Clear cart</a>
+
+                        @if (count($cartItems) > 0)
+                            <div class="col-lg-6 col-md-6 col-sm-6">
+                                <div class="continue__btn update__btn">
+                                    <button
+                                        style="background-color:rgb(255, 255, 255);border-radius:10px;padding:5px;border-color:red;"
+                                        wire:click="clear()">
+                                        <i style="color:rgb(255, 0, 0);" class="fa fa-trash"></i> Clear cart
+                                    </button>
+                                </div>
                             </div>
-                        </div>
+                        @endif
+
                     </div>
                 </div>
                 <div class="col-lg-4">
@@ -94,8 +106,7 @@
                     <div class="cart__total">
                         <h6>Cart total</h6>
                         <ul>
-                            {{-- <li>Subtotal <span>$ 169.50</span></li> --}}
-                            {{-- <li>Total <span>R{{ $sum }}</span></li> --}}
+                            <li>Total <span>R{{ $totalPrice }}</span></li>
                         </ul>
                         <a href="#" class="primary-btn">Proceed to checkout</a>
                     </div>
