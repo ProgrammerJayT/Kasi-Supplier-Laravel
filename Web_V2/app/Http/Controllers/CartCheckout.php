be4ac8b1f3a408f43ded809e15ab6e7c4263b73a
Programmer JayT <theanthem8@gmail.com>
2022-10-13 23:20:51 +0200

    Created show method

--- Web_V2/app/Http/Controllers/CartCheckout.php
+++ Web_V2/app/Http/Controllers/CartCheckout.php
@@ -7,4 +7,26 @@ use Illuminate\Http\Request;
 class CartCheckout extends Controller
 {
     //
+    public function show(Request $request)
+    {
+        $userInfo = User::show($request->user, session()->get('user'));
+
+        return view('checkout', [
+            'user' => $request->user,
+            'name' => $userInfo->name,
+            'image' => $userInfo->image,
+            'userInfo' => $userInfo,
+            'cart' => session()->has('cartItems') ? session()->get('cartItems') : array(),
+            'items' => ShoppingItems::index()->original,
+            'total' => $request->total
+        ]);
+    }
+
+    public function checkout(Request $request)
+    {
+        // $request->validate([
+        //     'card number' => 'required|size:16|numeric',
+        //     //'expiry' => 'min:now',
+        // ]);
+    }
 }
