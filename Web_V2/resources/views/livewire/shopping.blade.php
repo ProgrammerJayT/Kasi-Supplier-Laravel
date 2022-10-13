53de17466fdaf60a7b056b7d8fa12dd603633669
Programmer JayT <theanthem8@gmail.com>
2022-10-13 23:20:14 +0200

    Added H1 dev for fun

--- Web_V2/resources/views/livewire/shopping.blade.php
+++ Web_V2/resources/views/livewire/shopping.blade.php
@@ -5,6 +5,9 @@
             <div class="row">
                 <div class="col-lg-8">
                     <div class="checkout__input">
+
+                        <h1 style="color:red;">Development mode</h1>
+
                         <input wire:model="search" type="text" placeholder="Search for items here. E.g Tomatoes"
                             name="surname" value="{{ old('surname') }}">
 
