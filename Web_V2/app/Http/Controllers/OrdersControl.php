d0b6e54b19578f32c93bcacaacf7923f4cdd858c
Programmer JayT <theanthem8@gmail.com>
2022-10-15 10:57:52 +0200

    Created orders controller

---
+++ Web_V2/app/Http/Controllers/OrdersControl.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use Illuminate\Http\Request;
+
+class OrdersControl extends Controller
+{
+    //
+    public function show() {
+        $myID = session()->get('user')['id'];
+        $accountType = session()->get('user')['type'];
+
+        $user = User::show($accountType, $myID);
+
+        return view('orders', [
+            'name' => $user->name,
+            'image' => $user->image,
+            'user' => $accountType,
+        ]);
+    }
+}
