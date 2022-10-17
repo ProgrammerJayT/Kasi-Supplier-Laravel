992912795ca32b18c0df91e3ef5ba607da0fb3c9
Programmer JayT <theanthem8@gmail.com>
2022-10-17 23:42:31 +0200

    Modified show method to static

--- Web_V2/app/Http/Controllers/BankingControl.php
+++ Web_V2/app/Http/Controllers/BankingControl.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BankingDetails;
 use Illuminate\Http\Request;
 
 class BankingControl extends Controller
 {
     //
-    public function show() {
-        
+    public static function show($id) {
+        $bankingInfo = BankingDetails::where('user_account_id', $id)->first();
+
+        return $bankingInfo;
     }
 }
