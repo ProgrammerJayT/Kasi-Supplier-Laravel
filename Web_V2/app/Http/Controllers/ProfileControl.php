3e97a83c3e47dfcafc92405620044c5f5a5ab948
Programmer JayT <theanthem8@gmail.com>
2022-10-17 23:43:41 +0200

    Added update banking details

--- Web_V2/app/Http/Controllers/ProfileControl.php
+++ Web_V2/app/Http/Controllers/ProfileControl.php
@@ -3,19 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Admin;
-use App\Models\Customer;
+use App\Models\BankAccountType;
+use App\Models\BankingDetails;
 use App\Models\Vendor;
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use App\Http\Controllers\User;
+use App\Http\Controllers\Auth\AccountControl;
+use App\Http\Controllers\Stripe\BankCardControl;
+use App\Models\Bank;
+use Stripe\StripeClient;
 
 class ProfileControl extends Controller
 {
     //
 
-    public function show() {
+    public function show()
+    {
         $myID = session()->get('user')['id'];
         $accountType = session()->get('user')['type'];
 
         $userInfo = User::show($accountType, $myID);
+
+        //Get user account information
+        $accountInfo = AccountControl::show($userInfo->email);
+        $bankingInfo = BankingControl::show($accountInfo->id);
+
+        //Get bank names and account type for user's banking details creation
+        $banks = Bank::all();
+        $bankAccountTypes = BankAccountType::all();
 
         return view('profile', [
             'name' => $userInfo->name,
@@ -24,19 +40,27 @@ class ProfileControl extends Controller
             'date' => $userInfo->created_at,
             'image' => $userInfo->image,
             'user' => $accountType,
+            'accountInfo' => $accountInfo,
+            'banks' => $banks,
+            'bankAccountTypes' => $bankAccountTypes,
         ]);
     }
 
     public function updateProfilePicture(Request $request)
     {
+        $userID = session()->get('user')['id'];
+        $accountType = session()->get('user')['type'];
+
+        $userInfo = User::show($accountType, $userID);
+
         $request->validate([
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
 
-        $imageName = $request->email . '.' . $request->image->extension();
-        $request->image->move(public_path('images/profile-pictures/' . $request->user), $imageName);
+        $imageName = $userInfo->email . '.' . $request->image->extension();
+        $request->image->move(public_path('images/profile-pictures/' . $accountType), $imageName);
 
-        switch ($request->user) {
+        switch ($accountType) {
             case 'customer':
                 $model = new Customer;
                 break;
@@ -54,8 +78,8 @@ class ProfileControl extends Controller
                 break;
         }
 
-        $model::where('email', $request->email)->update([
-            'image' => 'images/profile-pictures/' . $request->user . '/' . $imageName,
+        $model::where('email', $userInfo->email)->update([
+            'image' => 'images/profile-pictures/' . $accountType . '/' . $imageName,
         ]);
 
         return back()
@@ -94,5 +118,58 @@ class ProfileControl extends Controller
             return back()
                 ->with('image-update-success', 'You have no profile picture to remove.');
         }
+    }
+
+    public function updateBankingDetails(Request $request)
+    {
+        $request->validate([
+            'bank' => 'required',
+            'bank_account_type' => 'required',
+            'bank_account_number' => 'required|numeric|digits:10',
+            'bank_card_number' => 'required|numeric|digits:16',
+            'bank_card_expiry' => 'required|date',
+            'bank_card_cvv' => 'required|numeric|digits:3',
+        ]);
+
+        $bankCardData = [
+            'card_number' => $request->bank_card_number,
+            'exp_month' => explode('-', $request->bank_card_expiry)[1],
+            'exp_year' => explode('-', $request->bank_card_expiry)[0],
+            'card_cvv' => $request->bank_card_cvv,
+        ];
+
+        $bankingDetailsData = [
+            'bank' => $request->bank,
+            'account_type' => $request->bank_account_type,
+            'account_number' => $request->bank_account_number,
+            'card_number' => $request->bank_card_number,
+            'exp_month' => explode('-', $request->bank_card_expiry)[1],
+            'exp_year' => explode('-', $request->bank_card_expiry)[0],
+            'card_cvv' => $request->bank_card_cvv,
+        ];
+
+        $createBankCard = BankCardControl::create($bankCardData);
+
+        if ($createBankCard->getStatusCode() == 201) {
+            $createBankingDetails = BankingDetailsControl::create($bankingDetailsData);
+
+            $createBankingDetails['status'] == 201 ? [
+                $attempt = 'banking-update-success',
+                $message = 'You have successfully updated your banking details.'
+            ] : [
+                $attempt = 'banking-update-fail',
+                $message = 'Something went wrong. Please try again.'
+            ];
+
+            return back()
+                ->with($attempt, $message);
+        } else {
+            return back()
+                ->with('banking-update-fail', $createBankCard->original['message']);
+        }
+    }
+
+    public function removeBankingDetails()
+    {
     }
 }
