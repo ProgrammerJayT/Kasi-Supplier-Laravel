edb29f0cfcd31e376ecb7f1d47c90e4c6c668636
Programmer JayT <theanthem8@gmail.com>
2022-10-19 21:55:09 +0200

    Modified table attributes

--- Web_V2/database/migrations/2022_09_26_170255_create_order_table.php
+++ Web_V2/database/migrations/2022_09_26_170255_create_order_table.php
@@ -15,12 +15,12 @@ class CreateOrderTable extends Migration
     {
         Schema::create('order', function (Blueprint $table) {
             $table->id();
-            $table->integer('cust_id');
-            $table->integer('ven_id');
+            $table->integer('buyer_id');
             $table->date('date');
             $table->double('amount');
+            $table->integer('num_items');
             $table->boolean('is_delivery')->default(0);
-            $table->boolean('status')->default(0);       
+            $table->string('status')->default('unpaid');
             $table->timestamps();
         });
     }
