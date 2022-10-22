f831d41491604d3c074b55b3f40962376de52692
Programmer JayT <theanthem8@gmail.com>
2022-10-22 20:33:35 +0200

    Declared user information

--- Web_V2/app/Http/Controllers/CartCheckout.php
+++ Web_V2/app/Http/Controllers/CartCheckout.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Stripe\BankCardControl;
+use App\Http\Controllers\Stripe\ChargeControl;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Stripe\StripeClient;
@@ -30,81 +32,55 @@ class CartCheckout extends Controller
 
     public function checkout(Request $request)
     {
+        //Initialize user info
         $myID = session()->get('user')['id'];
         $accountType = session()->get('user')['type'];
+        $userInfo = User::show($accountType, $myID);
 
         $request->validate([
             'cardNumber' => 'required|size:16',
             'cardExpiry' => 'required',
-            'cardCvv' => 'required|size:3',
+            'cardCvc' => 'required|size:3',
             'deliveryAddress' => 'required_if:deliveryChoice,==,true',
         ]);
 
-        //JSON decode address
-        $latLng = json_decode($request->input('set-address'), true);
-
-        //Initialize user info
-        $userInfo = User::show($accountType, $myID);
+        if ($request->deliveryChoice == 'on') {
+            $latLng = json_decode($request->input('set-address'), true);
+        }
 
         //Go reverse geocode to get address
         try {
             $response = Http::get(
                 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' . $latLng['lat'] . ',' . $latLng['lng'] . '&location_type=ROOFTOP&result_type=street_address&key=AIzaSyColBQqGsiDgBrMtcp3GSVbVQWOW9TNe_0'
             );
+
+            $rawAddress = $response->json()['results'][0]['address_components'];
         } catch (\Exception $e) {
             return redirect()->back()->with('error', $e->getMessage());
         }
 
-        $rawAddress = $response->json()['results'][0]['address_components'];
-        $streetAddress = $rawAddress[0]['long_name'] . ' ' . $rawAddress[1]['long_name'];
+        $createCardTokenData = [
+            'card_number' => $request->cardNumber,
+            'exp_month' => explode('-', $request->cardExpiry)[1],
+            'exp_year' => explode('-', $request->cardExpiry)[0],
+            'card_cvc' => $request->cardCvc,
+        ];
 
-        //Stripe
-        $stripeClient = new StripeClient([
-            'api_key' => env('STRIPE_SECRET'),
-        ]);
+        if (BankCardControl::create($createCardTokenData)->getStatusCode() == 201) {
+            $bankCardResponse = json_decode(BankCardControl::create($createCardTokenData)->content(), true);
+            $cardToken = $bankCardResponse['data']['id'];
 
-        //Create card token
-        try {
-            $token = $stripeClient->tokens->create([
-                'card' => [
-                    'number' => $request->cardNumber,
-                    'exp_month' => explode('-', $request->cardExpiry)[1],
-                    'exp_year' => explode('-', $request->cardExpiry)[0],
-                    'cvc' => $request->cardCvv,
-                ],
-            ]);
+            $createChargeData = [
+                'amount' => session()->get('new-order')['total'],
+                'email' => $userInfo->email,
+                'token' => $cardToken,
+                'address' => $rawAddress,
+                'name' => $userInfo->name . ' ' . $userInfo->surname,
+            ];
 
-
-            //Create charge
-            try {
-                $charge = $stripeClient->charges->create([
-                    'amount' => 1500,
-                    'currency' => 'zar',
-                    'source' => $token->id,
-                    'description' => 'Charge for ' . User::show($accountType, $myID)->email,
-                    'receipt_email' => User::show($accountType, $myID)->email,
-                    'shipping' => [
-                        'name' => $userInfo->name,
-                        'phone' => null,
-                        'address' => [
-                            'line1' => $streetAddress,
-                            'city' => $rawAddress[4]['long_name'],
-                            'state' => $rawAddress[5]['long_name'],
-                            'country' => $rawAddress[6]['long_name'],
-                            'postal_code' => $rawAddress[7]['long_name'],
-                        ],
-                    ],
-                ]);
-
-
-                //Display error for failed charge
-            } catch (\Exception $e) {
-                return redirect()->back()->with('error', $e->getMessage());
-            }
-
-            //Display error for card token creation failure21
-        } catch (\Exception $e) {
-            return redirect()->back()->with('error', $e->getMessage());
+            ChargeControl::create($createChargeData);
+        } else {
+            return back()->with('error', BankCardControl::create($createCardTokenData)->original['message']);
         }
     }
 
