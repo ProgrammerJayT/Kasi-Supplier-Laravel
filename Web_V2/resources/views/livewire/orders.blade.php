65b198d087d3c665ef55ca76ed15becb179371a1
Programmer JayT <theanthem8@gmail.com>
2022-10-23 17:12:00 +0200

    Modified orders filter

--- Web_V2/resources/views/livewire/orders.blade.php
+++ Web_V2/resources/views/livewire/orders.blade.php
@@ -1,6 +1,4 @@
 <div>
-
-
     <!-- Shopping Cart Section Begin -->
     <section class="shopping-cart spad">
         <div class="container">
@@ -8,28 +6,25 @@
                 <div class="col-lg-8">
                     <div class="checkout__input">
                         <div class="row">
-                            <div class="col-lg-9">
-                                <input wire:model="search" type="text" placeholder="Search for items here. E.g Tomatoes"
-                                    name="search">
-                            </div>
 
-                            <div class="col-lg-3">
-                                <select name="filter" id="type" value="">
-                                    <option value="">All orders</option>
-                                    <option value="vendor">My orders</option>
-                                    <option value="customer">Customer orders</option>
-                                </select>
-                            </div>
+                            @unless($orders->count() == 0)
+                                <div class="col-lg-9">
+                                    <input wire:model="search" type="text" placeholder="Search for order id" name="search">
+                                </div>
+
+                                <div class="col-lg-3">
+                                    <select wire:model="orderStatus" name="orderStatus" id="orderStatus">
+                                        <option value="">All orders</option>
+                                        <option value="canceled">Canceled orders</option>
+                                        <option value="unpaid">Unpaid orders</option>
+                                        <option value="paid">Paid orders</option>
+                                    </select>
+                                </div>
+                            @endunless
                         </div>
                     </div>
 
-                    @if (Session::has('item-remove') && count($cartItems) > 0)
-                        <div class="alert alert-success" role="alert">
-                            {{ Session::get('item-remove') }}
-                        </div>
-                    @endif
-
-                    @unless(count($cartItems) == 0)
+                    @unless($orders->count() > 0)
                         <div>
                             <h4 class="alert-heading">No orders</h4>
                             <p>Looks like you haven't placed any orders yet. Click <a
@@ -43,7 +38,7 @@
                                     <tr>
                                         <th>Order ID</th>
                                         <th>Date</th>
-                                        <th>NO. of items</th>
+                                        <th>#Items</th>
                                         <th>Amount</th>
                                         <th>Status</th>
                                         <th>Receipient</th>
@@ -51,105 +46,57 @@
                                 </thead>
                                 <tbody>
 
-                                    <tr>
-                                        <td class="quantity__item">
-                                            <div class="product__cart__item__pic">
-                                                <h6>#59</h6>
-                                            </div>
-                                        </td>
-                                        <td class="quantity__item">
-                                            <div class="product__cart__item__pic">
-                                                <h6>16/12/2022</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__pic">
-                                                <h6>13</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__pic">
-                                                <h6>R9210</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__pic">
-                                                <h6 style="color: green;">Complete</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__text">
-                                                <h6>Irvin Ngobeni</h6>
-                                                <p style="margin-bottom:0px;">irvin@kasi.com</p>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="quantity__item">
-                                            <div class="product__cart__item__pic">
-                                                <h6>#68</h6>
-                                            </div>
-                                        </td>
-                                        <td class="quantity__item">
-                                            <div class="product__cart__item__pic">
-                                                <h6>16/12/2022</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__pic">
-                                                <h6>11</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__pic">
-                                                <h6>R2210</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__pic">
-                                                <h6 style="color: red;">Canceled</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__text">
-                                                <h6>Irvin Ngobeni</h6>
-                                                <p style="margin-bottom:0px;">irvin@kasi.com</p>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="quantity__item">
-                                            <div class="product__cart__item__pic">
-                                                <h6>#98</h6>
-                                            </div>
-                                        </td>
-                                        <td class="quantity__item">
-                                            <div class="product__cart__item__pic">
-                                                <h6>16/12/2022</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__pic">
-                                                <h6>6</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__pic">
-                                                <h6>R5210</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__pic">
-                                                <h6 style="color: yellow;">Pending</h6>
-                                            </div>
-                                        </td>
-                                        <td class="cart__price">
-                                            <div class="product__cart__item__text">
-                                                <h6>Irvin Ngobeni</h6>
-                                                <p style="margin-bottom:0px;">irvin@kasi.com</p>
-                                            </div>
-                                        </td>
-                                    </tr>
+                                    @foreach ($orders as $order)
+                                        <tr>
+                                            <td class="quantity__item">
+                                                <div class="product__cart__item__pic">
+                                                    <h6>#{{ $order->id }}</h6>
+                                                </div>
+                                            </td>
+                                            <td class="quantity__item">
+                                                <div class="product__cart__item__pic">
+                                                    <h6>{{ $order->created_at->toDateString() }}</h6>
+                                                </div>
+                                            </td>
+                                            <td class="cart__price">
+                                                <div class="product__cart__item__pic">
+                                                    <h6>{{ $order->num_items }}</h6>
+                                                </div>
+                                            </td>
+                                            <td class="cart__price">
+                                                <div class="product__cart__item__pic">
+                                                    <h6>R{{ $order->amount }}</h6>
+                                                </div>
+                                            </td>
+
+                                            @if ($order->status == 'pending')
+                                                <td class="cart__price">
+                                                    <div class="product__cart__item__pic">
+                                                        <h6 class="text-warning">Pending</h6>
+                                                    </div>
+                                                </td>
+                                            @elseif($order->status == 'unpaid')
+                                                <td class="cart__price">
+                                                    <div class="product__cart__item__pic">
+                                                        <h6 class="text-danger">Unpaid</h6>
+                                                    </div>
+                                                </td>
+                                            @elseif($order->status == 'paid')
+                                                <td class="cart__price">
+                                                    <div class="product__cart__item__pic">
+                                                        <h6 class="text-success">Paid</h6>
+                                                    </div>
+                                                </td>
+                                            @endif
+
+                                            <td class="cart__price">
+                                                <div class="product__cart__item__text">
+                                                    <h6>{{ $user->name . ' ' . $user->surname }}</h6>
+                                                    <p style="margin-bottom:0px;">{{ $user->email }}</p>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
                         </div>
@@ -165,23 +112,26 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="cart__total">
-                        <h2>Orders</h2>
-                        <h4>Your orders - 32 in total</h4>
-                        <ul>
-                            <li>Successful <span>13</span></li>
-                            <li>Pending <span>9</span></li>
-                            <li>Canceled <span>10</span></li>
-                        </ul>
-                        <h4>Customer orders - 15 in total</h4>
-                        <ul>
-                            <li>Successful <span>9</span></li>
-                            <li>Pending <span>1</span></li>
-                            <li>Canceled <span>5</span></li>
-                        </ul>
+
+                @unless($orders->count() == 0)
+                    <div class="col-lg-4">
+                        <div class="cart__total">
+                            <h2>Orders</h2>
+                            <h4>Your orders - {{ $orders->count() }} in total</h4>
+                            <ul>
+                                <li>Successful <span>13</span></li>
+                                <li>Pending <span>9</span></li>
+                                <li>Canceled <span>10</span></li>
+                            </ul>
+                            {{-- <h4>Customer orders - 15 in total</h4>
+                            <ul>
+                                <li>Successful <span>9</span></li>
+                                <li>Pending <span>1</span></li>
+                                <li>Canceled <span>5</span></li>
+                            </ul> --}}
+                        </div>
                     </div>
-                </div>
+                @endunless
             </div>
         </div>
     </section>
