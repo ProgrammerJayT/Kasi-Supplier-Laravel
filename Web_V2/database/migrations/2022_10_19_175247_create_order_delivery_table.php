06568f5a176c37cb48c8972578998551541f7384
Programmer JayT <theanthem8@gmail.com>
2022-10-23 05:47:39 +0200

    Added latlng attribute

--- Web_V2/database/migrations/2022_10_19_175247_create_order_delivery_table.php
+++ Web_V2/database/migrations/2022_10_19_175247_create_order_delivery_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->integer('order_id');
             $table->string('address');
+            $table->string('latlng');
             $table->string('distance');
             $table->timestamps();
         });
